d03cfeb5e498d8c02b929e3b1f89936608b3df97
wyuhua <815630330@qq.com>
2022-12-06 16:56:36 +0800

    add ImGui

--- OpenglLearn/Src/Application.cpp
+++ OpenglLearn/Src/Application.cpp
@@ -17,6 +17,11 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+#include "tests/TestClearColor.h"
 
 int main(void)
 {
@@ -29,7 +34,6 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
@@ -51,87 +55,51 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     {
-        //vertex data
-        float positions[] = {
-                    100.0f, 100.0f, 0.0f, 0.0f,//0
-                    200.0f, 100.0f, 1.0f, 0.0f,//1
-                    200.0f, 200.0f, 1.0f, 1.0f,//2
-                    100.0f, 200.0f, 0.0f, 1.0f//3
-        };
-
-        //vertex indexs
-        unsigned int indices[] = {
-            0, 1, 2,
-            2, 3, 0
-        };
-
-        GLCall(glEnable(GL_BLEND));
-        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-        //vetexbuffer
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-        //vertexbuffer layout
-        VertexBufferLayout layout;
-        layout.Push<float>(2);
-        layout.Push<float>(2);
-
-        //vetextArray
-        VertexArray va;
-        va.AddBuffer(vb, layout);
-
-        //indexBuffer
-        IndexBuffer ib(indices, 6);
-
-        //mvp
-        glm::mat4 proj = glm::ortho(0.0f, 960.f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100.0f, 0.0f, 0.0f));
-
-        glm::mat4 mvp = proj * view;
-
-        //shader
-        Shader shader("res/shaders/Basic.shader");
-        shader.Bind();
-        shader.SetUniform4f("u_Color", 1.0f, 0.0f, 0.0f, 1.0f);
-
-        //texture
-        Texture texture("res/textures/3.png");
-        texture.Bind();
-        shader.SetUniform1i("u_Texture", 0);
-        shader.SetUniformMat4f("u_MVP", mvp);
-
-        //reset state
-        va.Unbind();
-        vb.Unbind();
-        ib.Unbind();
-        shader.Unbind();
-
         Renderer render;
-       
-        float r = 0.0f;
-        float increment = 0.05f;
+
+        //ImGui
+        ImGui::CreateContext();
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        const char* glsl_version = "#version 150"; //notice
+        ImGui_ImplOpenGL3_Init(glsl_version);
+        ImGui::StyleColorsDark();
+
+        test::Test* currentTest = nullptr;
+        test::TestMenu* testMenu = new test::TestMenu(currentTest);
+        currentTest = testMenu;
+
+        testMenu->RegisterTest<test::TestClearColor>("Clear Color");
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
+            GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
             render.Clear();
 
-            shader.Bind();
-            shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-      
-            render.Draw(va, ib, shader);
+            // Start the Dear ImGui frame
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
 
-            if (r > 1.0f)
+            if (currentTest)
             {
-                increment = -0.05f;
-            }
-            else if (r < 0.0f)
-            {
-                increment = 0.05f;
+                currentTest->OnUpdate(0.0f);
+                currentTest->OnRender();
+                ImGui::Begin("Test");
+                if (currentTest != testMenu && ImGui::Button("<-"))
+                {
+                    delete currentTest;
+                    currentTest = testMenu;
+                }
+
+                currentTest->OnImGuiRender();
+                ImGui::End();
             }
 
-            r += increment;
+            // Rendering
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
@@ -142,6 +110,11 @@ int main(void)
             glfwPollEvents();
         }
     }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     glfwTerminate();
 
---
+++ OpenglLearn/Src/tests/TestClearColor.cpp
@@ -0,0 +1,33 @@
+#include "TestClearColor.h"
+
+#include "GL/glew.h"
+#include "Renderer.h"
+#include "imgui/imgui.h"
+
+namespace test
+{
+	test::TestClearColor::TestClearColor() : m_ClearColor{0.0f, 0.0f, 0.0f, 1.0f}
+	{
+	}
+
+	TestClearColor::~TestClearColor()
+	{
+	}
+
+	void TestClearColor::OnUpdate(float deltalTime)
+	{
+	}
+
+	void TestClearColor::OnRender()
+	{
+		GLCall(glClearColor(m_ClearColor[0], m_ClearColor[1], m_ClearColor[2], m_ClearColor[3]));
+		GLCall(glClear(GL_COLOR_BUFFER_BIT));
+	}
+
+	void TestClearColor::OnImGuiRender()
+	{
+		ImGui::ColorEdit4("Clear Color", m_ClearColor);
+	}
+
+}
+
